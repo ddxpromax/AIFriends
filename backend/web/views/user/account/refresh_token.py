0ea8fedf5781677bb293726262783618c4f944b1
ddx <ddx@localhost>
2026-07-11 14:21:52 +0800

    第一次打开页面或刷新页面时，从云端动态拉取用户信息

--- backend/web/views/user/account/refresh_token.py
+++ backend/web/views/user/account/refresh_token.py
@@ -13,7 +13,7 @@ class RefreshTokenView(APIView):
                     'result': 'refresh_token_not_exist'
                 }, status=401)
             refresh = RefreshToken(refresh_token)
-            if settings.SIMPLE_JWT['ROTATE_REFRESH_TOKEN']:
+            if settings.SIMPLE_JWT['ROTATE_REFRESH_TOKENS']:
                 refresh.set_jti()
                 return Response({
                     'result': 'success',
