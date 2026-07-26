6a28e162c1565c5965d56489dfdc2d7d09b6a93d
ddx <ddx@localhost>
2026-07-26 12:16:40 +0800

    实现搜索功能

--- frontend/src/views/homepage/HomepageIndex.vue
+++ frontend/src/views/homepage/HomepageIndex.vue
@@ -1,12 +1,14 @@
 <script setup>
-import {nextTick, onMounted, ref, useTemplateRef, onBeforeUnmount} from "vue";
+import {nextTick, onMounted, ref, useTemplateRef, onBeforeUnmount, watch} from "vue";
 import api from "@/js/http/api.js"
 import Character from "@/components/character/Character.vue";
+import { useRoute } from "vue-router";
 
 const characters = ref([])
 const isLoading = ref(false)
 const hasCharacters = ref(true)
 const sentinelRef = useTemplateRef('sentinel-ref')
+const route = useRoute()
 
 function checkSentinelVisible() {
   if (!sentinelRef.value) return false
@@ -23,7 +25,8 @@ async function loadMore() {
     try {
         const res = await api.get('/api/homepage/index/', {
             params: {
-                'items_count': characters.value.length
+                items_count: characters.value.length,
+                search_query: route.query.q || ''
             }
         })
         const data = res.data
@@ -62,6 +65,17 @@ onMounted(async () => {
   )
 
   observer.observe(sentinelRef.value)
+})
+
+function reset() {
+    characters.value = []
+    isLoading.value = false
+    hasCharacters.value = true
+    loadMore()
+}
+
+watch(() => route.query.q, newQ => {
+    reset()
 })
 
 onBeforeUnmount(() => {
