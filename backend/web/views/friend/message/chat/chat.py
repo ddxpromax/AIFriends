901f1d62d05862068afe14dabc3bdfbbbdf2feeb
ddx <ddx@localhost>
2026-09-03 19:50:38 +0800

    添加系统提示词和多轮对话

--- backend/web/views/friend/message/chat/chat.py
+++ backend/web/views/friend/message/chat/chat.py
@@ -2,7 +2,7 @@ import json
 
 from django.http import StreamingHttpResponse
 
-from web.models.friend import Message
+from web.models.friend import Message, SystemPrompt
 
 from rest_framework.renderers import BaseRenderer
 from rest_framework.views import APIView
@@ -12,8 +12,7 @@ from rest_framework.permissions import IsAuthenticated
 from web.models.friend import Friend
 from web.views.friend.message.chat.graph import ChatGraph
 
-from langchain_core.messages import HumanMessage
-from langchain_core.messages import BaseMessageChunk
+from langchain_core.messages import HumanMessage, BaseMessageChunk, SystemMessage, AIMessage
 
 class SSERenderer(BaseRenderer):
     media_type = 'text/event-stream'
@@ -21,6 +20,24 @@ class SSERenderer(BaseRenderer):
     def render(self, data, accepted_media_type=None, renderer_context=None):
         return data
 
+def add_system_prompt(state, friend):
+    msgs = state['messages']
+    system_prompts = SystemPrompt.objects.filter(title='回复').order_by('order_number')
+    prompt = ''
+    for sp in system_prompts:
+        prompt += sp.prompt
+    prompt += f'\n【角色性格】\n{friend.character.profile}\n'
+    return {'messages': [SystemMessage(prompt)] + msgs}
+
+def add_recent_messages(state, friend):
+    msgs = state['messages']
+    message_raw = list(Message.objects.filter(friend=friend).order_by('-id')[:10])
+    message_raw.reverse()
+    messages = []
+    for m in message_raw:
+        messages.append(HumanMessage(m.user_message))
+        messages.append(AIMessage(m.output))
+    return {'messages': msgs[:1] + messages + msgs[-1:]}
 
 class MessageChatView(APIView):
     permission_classes = [IsAuthenticated]
@@ -43,6 +60,8 @@ class MessageChatView(APIView):
         inputs = {
             'messages': [HumanMessage(message)]
         }
+        inputs = add_system_prompt(inputs, friend)
+        inputs = add_recent_messages(inputs, friend)
 
         def event_stream():
             full_output = ''
